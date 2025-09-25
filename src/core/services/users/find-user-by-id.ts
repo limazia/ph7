9b91a580b4c8999b4235931ee87ddf075d52977c
Acacio de Lima <limadeacacio@gmail.com>
2025-09-24 23:16:21 -0300

    feat(users): improve find user by ID functionality with enhanced error handling and return user details

--- src/core/services/users/find-user-by-id.ts
+++ src/core/services/users/find-user-by-id.ts
@@ -1,11 +1,32 @@
+import { prisma } from "@/lib/prisma";
+import { User, UserParamsDto } from "@/core/model/users.model";
 import { HttpError } from "@/lib/http-error";
 
 export class FindUserById {
-  async execute({ id }: { id: string }) {
+  async execute({ id }: UserParamsDto): Promise<User> {
     try {
-      return { id };
+      const user = await prisma.user.findUnique({
+        where: { id },
+        select: {
+          id: true,
+          email: true,
+          name: true,
+          role: true,
+          createdAt: true,
+          updatedAt: true,
+        },
+      });
+
+      if (!user) {
+        throw new HttpError("Usuário não encontrado", 404, "not_found");
+      }
+
+      return user as User;
     } catch (error) {
-      throw new HttpError("Erro ao buscar o usuário.");
+      if (error instanceof HttpError) {
+        throw error;
+      }
+      throw new HttpError("Erro ao buscar o usuário", 500, "internal_error");
     }
   }
 }
